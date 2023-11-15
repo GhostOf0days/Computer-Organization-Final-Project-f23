f2a2648d19e3fefdc347ea35c86ff7546818be7b
GhostOf0days <jaisal.patel45@gmail.com>
2023-11-15 18:22:25 -0500

    Added useful scripts

---
+++ scripts/createSolutionsDocument.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to the tests directory
+cd solutions
+
+# Create or clear the solutions.txt file
+> solutions.txt
+
+# Loop through all the solution files
+for solution in solution*.c; do
+  # Add a header for the tsolutionest file in solutions.txt
+  echo "===== Content of $solution =====" >> solutions.txt
+
+  # Append the content of the test file to solutions.txt
+  cat "$solution" >> solutions.txt
+
+  # Optionally, add a separator or newline for readability
+  echo -e "\n\n" >> solutions.txt
+done
+
+# Navigate back to the original directory
+cd ..
---
+++ scripts/runTests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Used to automatically check all input against solutions without manually doing it.
+
+# Navigate to the tests directory
+cd tests
+
+# Loop through all the test files
+for testfile in test*.c; do
+  echo "Running test: $testfile"
+  
+  # Extract the base number of the test file for matching with the solution
+  testnum=${testfile#test}
+  testnum=${testnum%.c}
+
+  # Run the program with the test file and capture its output
+  ../main.out "$testfile" > ../test_output.txt
+
+  # Now compare the output with the corresponding solution file
+  if diff -w -B ../test_output.txt ../solutions/solution${testnum}.c > /dev/null; then
+    echo "Test $testnum PASS"
+  else
+    echo "Test $testnum FAIL"
+    # Uncomment the line below if you want to see the diff for failed tests
+    # diff -w -B ../test_output.txt ../solutions/solution${testnum}.c
+  fi
+done
+
+# Clean up: remove the generated test output file
+rm ../test_output.txt
+
+# Navigate back to the original directory
+cd ..
