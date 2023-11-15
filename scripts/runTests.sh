d1d61a6ca61f62714014ee9f507b60722c4408a6
GhostOf0days <jaisal.patel45@gmail.com>
2023-11-15 18:25:49 -0500

    Added structure for testing

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -6,23 +6,23 @@
 cd tests
 
 # Loop through all the test files
-for testfile in test*.c; do
+for testfile in test*.txt; do
   echo "Running test: $testfile"
   
   # Extract the base number of the test file for matching with the solution
   testnum=${testfile#test}
-  testnum=${testnum%.c}
+  testnum=${testnum%.txt}
 
   # Run the program with the test file and capture its output
   ../main.out "$testfile" > ../test_output.txt
 
   # Now compare the output with the corresponding solution file
-  if diff -w -B ../test_output.txt ../solutions/solution${testnum}.c > /dev/null; then
+  if diff -w -B ../test_output.txt ../solutions/solution${testnum}.txt > /dev/null; then
     echo "Test $testnum PASS"
   else
     echo "Test $testnum FAIL"
     # Uncomment the line below if you want to see the diff for failed tests
-    # diff -w -B ../test_output.txt ../solutions/solution${testnum}.c
+    # diff -w -B ../test_output.txt ../solutions/solution${testnum}.txt
   fi
 done
 
