9ed4c6a265c4bb6ac3df7878ec33ef059e98a1a0
GhostOf0days <jaisal.patel45@gmail.com>
2023-12-01 13:05:33 -0500

    Reset design

--- verilog files/design.v
+++ verilog files/design.v
@@ -1,5 +1,107 @@
 `timescale 1ns / 1ps // Set the timescale to 1 nanosecond / 1 picosecond
 
+// Accumulator
+module Accumulator (
+    input wire clk, // Clock
+    input wire reset, // Reset
+    input wire [15:0] data_in, // Data input (16 bits)
+    output reg [15:0] acc // Accumulator (16 bits)
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin // If reset is asserted, reset the accumulator
+        acc <= 16'b0; 
+    end else begin // Otherwise, add data_in to the accumulator
+        acc <= acc + data_in;
+    end
+end
+
+endmodule
+
+
+// Program Counter
+module ProgramCounter (
+    input wire clk, // Clock
+    input wire reset, // Reset
+    input wire jump, // Jump enable
+    input wire [15:0] jump_addr, // Jump address (16 bits)
+    output reg [15:0] pc // Program counter (16 bits)
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin // If reset is asserted, reset the program counter
+        pc <= 16'b0;
+    end else if (jump) begin // If jump is asserted, jump to the address specified by jump_addr
+        pc <= jump_addr;
+    end else begin // Otherwise, increment the program counter
+        if (pc == 16'hFFFF) begin  // If the program counter is at its maximum value, wrap around to 0
+            $display("Warning: Program Counter overflow");
+            pc <= 16'b0;
+        end else begin // Otherwise, increment the program counter
+            pc <= pc + 1;
+        end
+    end
+end
+
+endmodule
+
+
+// Memory Access Register
+module MAR (
+    input wire clk, // Clock
+    input wire reset, // Reset
+    input wire [15:0] addr_in, // Address input (16 bits)
+    output reg [15:0] addr_out // Address output (16 bits)
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin // If reset is asserted, reset the address output
+        addr_out <= 16'b0;
+    end else begin // Otherwise, set the address output to the address input
+        addr_out <= addr_in;
+    end
+end
+
+endmodule
+
+
+// Memory Buffer Register
+module MBR (
+    input wire clk, // Clock
+    input wire reset, // Reset
+    input wire [15:0] data_in, // Data input (16 bits)
+    output reg [15:0] data_out // Data output (16 bits)
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin // If reset is asserted, reset the data output
+        data_out <= 16'b0;
+    end else begin // Otherwise, set the data output to the data input
+        data_out <= data_in;
+    end
+end
+
+endmodule
+
+
+// Instruction Register
+module IR (
+    input wire clk, // Clock
+    input wire reset, // Reset
+    input wire [15:0] instr_in, // Instruction input (16 bits)
+    output reg [15:0] instr_out // Instruction output (16 bits)
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin // If reset is asserted, reset the instruction output
+        instr_out <= 16'b0;
+    end else begin // Otherwise, set the instruction output to the instruction input
+        instr_out <= instr_in;
+    end
+end
+
+endmodule
+
 parameter CLOCK_PERIOD = 100;  // Default clock period in nanoseconds
 
 // Clock
