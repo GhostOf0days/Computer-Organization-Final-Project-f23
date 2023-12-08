8d57ac28707762dd52b895c3480e09afe43447d1
Viane Matsibekker <117249183+04vmatsibekker@users.noreply.github.com>
2023-12-08 09:49:10 -0500

    fix the ram errors in the example

--- src/ram-example.v
+++ src/ram-example.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1 ps
 
 module ram 
-    # (parameter ADDR_WIDTH = 12,
+  # (parameter ADDR_WIDTH = 13,
        parameter DATA_WIDTH = 8,
        parameter LENGTH = (1<<ADDR_WIDTH)
 )
@@ -14,19 +14,22 @@ module ram
      input oe // Output enable
     );
 
-    reg [DATA_WIDTH-1:0] mem[LENGTH];
+    reg [DATA_WIDTH-1:0] mem [0:LENGTH-1];
+  
     reg [DATA_WIDTH-1:0] tmp_data;
+  	wire [DATA_WIDTH-1:0] output_data;
 
-    always @ (pasedge clk) begin
-        if (cs & we)
-            mem[addr] <= data;
+    always @ (posedge clk) begin
+        if (cs & we) 
+            mem[addr] <= data;     
     end
 
-    always @ (negege clk) begin // Negative edge, so no clock delay in reading. Not a big deal. Value that is read is on negative edge for this example.
-        if (cs & !we) 
-            tmp_data <= mem[addr];
+    always @ (negedge clk) begin
+      if (cs & !we)
+            tmp_data <= mem[addr]; 
     end
 
-    assign data cs & oe & !we ? tmp_data : 'hz;
+     assign output_data = cs && oe && !we ? tmp_data : 'hz;
+     assign data = output_data;
 
 endmodule
